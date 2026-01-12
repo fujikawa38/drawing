075017c1d4f6d726a468ddea8bcfba8e531a0940
fujikawa38 <tacchi.juc.08lastwin@gmail.com>
2026-01-13 00:13:37 +0900

    SpringSecurityの設定とログイン画面の作成

--- src/main/java/com/example/drawing/controller/LotteryController.java
+++ src/main/java/com/example/drawing/controller/LotteryController.java
@@ -2,6 +2,8 @@ package com.example.drawing.controller;
 
 import java.util.List;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +17,7 @@ import com.example.drawing.entity.Lottery;
 import com.example.drawing.entity.LotteryItem;
 import com.example.drawing.entity.User;
 import com.example.drawing.repository.LotteryRepository;
+import com.example.drawing.repository.UserRepository;
 import com.example.drawing.service.LotteryService;
 
 import lombok.RequiredArgsConstructor;
@@ -25,6 +28,7 @@ import lombok.RequiredArgsConstructor;
 public class LotteryController {
 
 	private final LotteryRepository lotteryRepository;
+	private final UserRepository userRepository;
 	private final LotteryService lotteryService;
 
 	@GetMapping("/{id}")
@@ -49,8 +53,10 @@ public class LotteryController {
 
 	@GetMapping
 	public String list(Model model) {
-		User user = new User();
-		user.setId(1L);
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+		String username = auth.getName();
+		User user = userRepository.findByUsername(username).orElseThrow();
 
 		List<Lottery> lotteries = lotteryRepository.findByUser(user);
 
@@ -72,6 +78,32 @@ public class LotteryController {
 		user.setId(1L);
 		lottery.setUser(user);
 		lotteryRepository.save(lottery);
+		return "redirect:/lotteries";
+	}
+
+	@GetMapping("/{id}/edit")
+	public String edit(@PathVariable Long id, Model model) {
+		Lottery lottery = lotteryRepository.findById(id).orElseThrow();
+		model.addAttribute("lottery", lottery);
+		return "lottery/edit";
+	}
+
+	@PostMapping("/{id}")
+	public String update(@PathVariable Long id, @ModelAttribute Lottery form) {
+		Lottery lottery = lotteryRepository.findById(id).orElseThrow();
+
+		lottery.setTitle(form.getTitle());
+		lottery.setExcludeType(form.getExcludeType());
+		lottery.setExcludeCount(form.getExcludeCount());
+		lottery.setExcludeDays(form.getExcludeDays());
+
+		lotteryRepository.save(lottery);
+		return "redirect:/lotteries";
+	}
+
+	@PostMapping("/{id}/delete")
+	public String delete(@PathVariable Long id) {
+		lotteryRepository.deleteById(id);
 		return "redirect:/lotteries";
 	}
 
